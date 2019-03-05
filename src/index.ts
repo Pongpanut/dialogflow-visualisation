a212581e309de0ecc11b97c0781a593f45e005cd
pongpanut <pongpanut@gmail.com>
2019-03-05 17:24:15 +0700

    Remove firebase function and initiate HTML compose logic

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,113 @@
-// import * as functions from 'firebase-functions';
 import { IIntent } from './interface/IIntent';
 
 const dialogflow = require('dialogflow');
-const uuid = require('uuid');
 const express = require('express')
 const app = express()
 const stringUtils = require('./Utils/StringUtils');
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   runSample('x-petch').then(() => console.log('this will succeed')).catch(() => console.log('obligatory catch'));
-//   response.send('Hello Petch. This is updated. with runSample ');
-// });
-
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) =>  renderHtml(res));
 app.get('/matchintent', (req, res) => runSample('x-petch').then(() => console.log('Success')).catch(() => console.log('obligatory catch')));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+function renderHtml(res){
+    res.set('Content-Type', 'text/html');
+    var htmlText = buildHtml();
+    res.send(new Buffer(htmlText));
+}
+
+function buildHtml(){
+    var text = '';
+    text += '<!DOCTYPE HTML>'
+    text += '<html>'
+    text += '<head>'
+    text += '<title>Timeline basic demo</title>'
+    text +='<script src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis.min.js"></script>'
+    text +='<link href="https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis.min.css" rel="stylesheet" type="text/css" />'
+    text +='<style type="text/css">'
+    text +='  body, html {'
+    text +='   font-family: sans-serif;'
+    text +='}'
+    text +='</style>'
+    text += '</head>'
+    text += '<body>'
+    text += '<div id="mynetwork"></div>'
+    text += '<div>Testtt</div>'
+    text += '<script type="text/javascript">'
+    text += 'var container = document.getElementById("mynetwork");'
+    text += 'var nodes = new vis.DataSet(['
+    text += '  {id: 11, label: "Node 11", level:1 ,title: "Tooltip for Node 11"},'
+    text +=   '{id: 12, label: "Node 12", level:2 , title: "Tooltip for Node 12"},'
+    text +=   '{id: 13, label: "Node 13", level:2 ,title: "Tooltip for Node 13"},'
+    text +=   '{id: 14, label: "Node 14", level:3 ,title: "Tooltip for Node 14"},'
+    text +=   '{id: 15, label: "Node 15", level:3 , title: "Tooltip for Node 15"}'
+    text += ']);'
+    text += 'var edges = new vis.DataSet(['
+    text += '{from: 11, to: 12,  title:"edge1"},'
+    text += '{from: 11, to: 13},'
+    text += '{from: 12, to: 14},'
+    text += '{from: 12, to: 15},'
+    text += ']);'
+    text += 'var data = {'
+    text +=   'nodes: nodes,'
+    text +=   'edges: edges'
+    text += '};'
+    text += buildOptionText()
+    text += 'var timeline = new vis.Network(container, data, options);'
+    text += '</script>'
+    text += '</body>'
+    text += '</html>'
+    return text;
+}
+
+function buildOptionText(){
+    return  'var options = {'
+        + 'nodes:{'
+     + 'shape: "box",'
+     + '},'
+     + 'interaction:{'
+     + 'dragNodes:true,'
+     + 'dragView: true,'
+     + 'hideEdgesOnDrag: false,'
+     + 'hideNodesOnDrag: false,'
+     + 'hover: true,'
+     + 'hoverConnectedEdges: false,'
+     + 'keyboard: {'
+     + 'enabled: false,'
+     + 'speed: {x: 10, y: 10, zoom: 0.02},'
+     + 'bindToWindow: true'
+     + '},'
+     + 'multiselect: false,'
+     + 'navigationButtons: false,'
+     + 'selectable: true,'
+     + 'selectConnectedEdges: false,'
+     + 'tooltipDelay: 300,'
+     + 'zoomView: true'
+     + '},'
+     + 'edges:{'
+     + 'color: "red",'
+     + 'arrows: {'
+     + 'to:     {enabled: true, scaleFactor:1, type:"arrow"},'
+     + 'middle: {enabled: false, scaleFactor:1, type:"arrow"},'
+     + 'from:   {enabled: false, scaleFactor:1, type:"arrow"}'
+     + '},'
+     + '},'
+     + 'layout: {'
+     + 'randomSeed: undefined,'
+     + 'improvedLayout:true,'
+     + 'hierarchical: {'
+     + 'enabled:true,'
+     + 'levelSeparation: 150,'
+     + 'nodeSpacing: 100,'
+     + 'treeSpacing: 200,'
+     + 'blockShifting: true,'
+     + 'edgeMinimization: true,'
+     + 'parentCentralization: true,'
+     + 'direction: "UD",'
+     + 'sortMethod: "hubsize"'
+     + '}'
+     + '}'
+     + '};'
+}
 
 async function runSample(projectId = 'your-project-id') {  
     listIntents('x-petch').then(() => console.log('listIntents Success')).catch(() => console.log('obligatory catch'))
@@ -51,40 +144,5 @@ async function listIntents(projectId){
     }
   } catch (err) {
     console.error('ERROR:', err);
-  }
-
-//   console.log(responses[0]);   
-}
-
-async function detectIntent(projectId){
-    const sessionId = uuid.v4();
-    const sessionClient = new dialogflow.SessionsClient();
-    const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-    const request = {
-       session: sessionPath,
-       queryInput: {
-         text: {
-           text: 'ดูหนัง',
-           languageCode: 'en-US',
-         },
-       },
-     };
-     let responses 
-     try {
-       responses = await sessionClient.detectIntent(request);
-     } catch (err) {
-       console.error('ERROR:', err);
-     }
-
-     console.log(responses); 
-     console.log('Detected intent');
-     const result = responses[0].queryResult;
-     console.log('Query:' +result.queryText);
-     console.log('OutputContexts:' +JSON.stringify(result.outputContexts));
-     console.log('Response: ${result.fulfillmentText}');
-     if (result.intent) {
-       console.log('Intent: '+result.intent.displayName);
-     } else {
-       console.log('No intent matched.');
-     }
+  }  
 }
